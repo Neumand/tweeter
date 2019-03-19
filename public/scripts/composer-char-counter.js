457460c1c026f9c96e477b70e1cd0172f9de09a7
David <david.m.neuman@gmail.com>
2019-03-19 22:09:55 +0000

    Added functionality to countdown the counter and show red, negative values when it is less than 0.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
   console.log("Working as intended!");
   $(".input-tweet").keyup(function() {
     let charRemaining = 140 - this.value.length;
+    let counter = $(this).siblings().find('.counter').text(charRemaining);
+    if (charRemaining < 0) {
+      $("#tweet-counter").css('color', 'red');
+    }
   });
 });
