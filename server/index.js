8d7e9d1aa7f3a4f19c36708204ebd92739b251ba
David <david.m.neuman@gmail.com>
2019-03-21 18:48:19 +0000

    Updated to connect to Mongo and pass it to the data helper factory function instead.

--- server/index.js
+++ server/index.js
@@ -11,7 +11,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect to ${MONGODB_URI}.`);
+    throw err;
+  }
+
+  console.log(`Connected to mongodb: ${MONGODB_URI}.`)
+
+  DataHelpers(db);
+})
+
+// const db = require("./lib/in-memory-db");
+
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
