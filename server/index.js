8ccdcc43076c32d6e2cc0e7679bc6b531337a757
David <david.m.neuman@gmail.com>
2019-03-21 19:09:15 +0000

    Modified code to correctly use other declared variables within MongoClient.

--- server/index.js
+++ server/index.js
@@ -2,17 +2,17 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
@@ -21,12 +21,15 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     throw err;
   }
 
-  console.log(`Connected to mongodb: ${MONGODB_URI}.`)
-
-  DataHelpers(db);
-})
-
-// const db = require("./lib/in-memory-db");
+  console.log(`Connected to mongodb: ${MONGODB_URI}.`);
+  
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  app.use("/tweets", tweetsRoutes);
+  app.listen(PORT, () => {
+    console.log("Example app listening on port " + PORT);
+  });
+});
 
 
 // The `data-helpers` module provides an interface to the database of tweets.
@@ -36,15 +39,8 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
 //
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
 
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
-
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
-});
